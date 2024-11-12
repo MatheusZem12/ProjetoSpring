2f6843d1a2697efb4d7c370213a7292325ba3970
Matheus Zem <matheuslajazem@gmail.com>
2024-11-12 15:47:28 -0300

    Updated GamesService including  findByIdGame()

--- DSListGame/dslist/src/main/java/com/dslist/dslist/service/GamesService.java
+++ DSListGame/dslist/src/main/java/com/dslist/dslist/service/GamesService.java
@@ -1,7 +1,10 @@
 package com.dslist.dslist.service;
 
+import com.dslist.dslist.dto.GameDTO;
 import com.dslist.dslist.dto.GameMinDTO;
 import com.dslist.dslist.repository.GamesRepository;
+import org.springframework.transaction.annotation.Transactional;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,7 +14,14 @@ import java.util.List;
 public class GamesService {
     @Autowired
     private GamesRepository gamesRepository;
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAllGames(){
         return gamesRepository.findAll().stream().map(x -> new GameMinDTO(x)).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public GameDTO findByIdGame(Long id){
+        return gamesRepository.findById(id).map(GameDTO::new).orElseThrow(() -> new EntityNotFoundException("Game not found with id " + id));
     }
 }
