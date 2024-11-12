18e41200cdda96c0f1a52e96656e54d2d50bc988
Matheus Zem <matheuslajazem@gmail.com>
2024-11-12 15:46:45 -0300

    DTOs Updates Using BeanUtils

--- DSListGame/dslist/src/main/java/com/dslist/dslist/dto/GameDTO.java
+++ DSListGame/dslist/src/main/java/com/dslist/dslist/dto/GameDTO.java
@@ -1,5 +1,6 @@
 package com.dslist.dslist.dto;
 
+
 import com.dslist.dslist.entity.Games;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -7,15 +8,20 @@ import lombok.NoArgsConstructor;
 import org.springframework.beans.BeanUtils;
 
 @Data
-@AllArgsConstructor @NoArgsConstructor
-public class GameMinDTO {
+@AllArgsConstructor
+@NoArgsConstructor
+public class GameDTO {
     private Long id;
     private String title;
+    private String genre;
     private String shortDescription;
+    private String longDescription;
+    private String platform;
     private String imgUrl;
     private Integer launchYear;
+    private Double score;
 
-    public GameMinDTO(Games games){
+    public GameDTO(Games games) {
         BeanUtils.copyProperties(games,this);
     }
 }
