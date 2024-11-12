6bea6160f77b1779d2cfa86e41814b2e5462dd3e
Matheus Zem <matheuslajazem@gmail.com>
2024-11-12 11:28:00 -0300

    Controller complete for GET GameMinDTO

--- DSListGame/dslist/src/main/java/com/dslist/dslist/controller/GamesController.java
+++ DSListGame/dslist/src/main/java/com/dslist/dslist/controller/GamesController.java
@@ -1,4 +1,23 @@
 package com.dslist.dslist.controller;
 
+import com.dslist.dslist.dto.GameMinDTO;
+import com.dslist.dslist.service.GamesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/games")
 public class GamesController {
+
+    @Autowired
+    private GamesService gamesService;
+
+    @GetMapping
+    public List<GameMinDTO> findAllGames(){
+        return gamesService.findAllGames();
+    }
 }
