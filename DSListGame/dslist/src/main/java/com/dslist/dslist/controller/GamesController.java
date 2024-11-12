289a0fee35b5f12f4489aee724864d6ef6fd9a27
Matheus Zem <matheuslajazem@gmail.com>
2024-11-12 15:59:29 -0300

    Including findByIdGame in Controller

--- DSListGame/dslist/src/main/java/com/dslist/dslist/controller/GamesController.java
+++ DSListGame/dslist/src/main/java/com/dslist/dslist/controller/GamesController.java
@@ -1,9 +1,11 @@
 package com.dslist.dslist.controller;
 
+import com.dslist.dslist.dto.GameDTO;
 import com.dslist.dslist.dto.GameMinDTO;
 import com.dslist.dslist.service.GamesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +21,10 @@ public class GamesController {
     @GetMapping
     public List<GameMinDTO> findAllGames(){
         return gamesService.findAllGames();
+    }
+
+    @GetMapping("/{id}")
+    public GameDTO findByIdGame(@PathVariable Long id){
+        return gamesService.findByIdGame(id);
     }
 }
